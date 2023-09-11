f3b7237db362b2eb1e7b889467d926ddb5440adb
ChiefLopes <chieflopesekede@gmail.com>
2023-09-11 19:57:12 +0100

    added SearcBox

--- my-movie-app/src/App.js
+++ my-movie-app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import SearchBox from "./SearchBox";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold text-5xl m-5 text-yellow-800">Movies</h1>
+          <h1 className="font-bold text-5xl m-5 text-yellow-800">Movies</h1>
+          <SearchBox />
     </div>
   );
 }
--- my-movie-app/src/SearchBox.jsx
+++ my-movie-app/src/SearchBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBox = () => {
   return (
     <div>
-      
+      <input className='w-8 h-8' type="text" />
     </div>
   )
 }
