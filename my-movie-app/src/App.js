bc350b2cb34d6c63b4fae0d1c40b50b13f3702ac
ChiefLopes <chieflopesekede@gmail.com>
2023-09-11 19:54:02 +0100

    added SearcBox

--- my-movie-app/src/App.js
+++ my-movie-app/src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-    <h1></h1>
+      <h1 className="font-bold text-5xl m-5 text-yellow-800">Movies</h1>
     </div>
   );
 }
 
 export default App;
-
