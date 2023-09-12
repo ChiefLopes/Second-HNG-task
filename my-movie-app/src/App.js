7f6ca12732f9fa077a0db9fa3bff036ea58506b5
ChiefLopes <chieflopesekede@gmail.com>
2023-09-12 12:23:24 +0100

    set the grid layout to 5x2

--- my-movie-app/src/App.js
+++ my-movie-app/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import MovieBanner from "./Components/MovieBanner";
 import Movies from "./Components/Movies";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl">Movies</h1>
+      <MovieBanner />
       <Movies />
     </div>
   );
--- my-movie-app/src/Components/MovieBanner.jsx
+++ my-movie-app/src/Components/MovieBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieBanner = () => {
   return (
     <div>
-      
+      Movies
     </div>
   )
 }
