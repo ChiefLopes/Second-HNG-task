7fb65b1b6c411309f4b5c3a7cec807d3a581ef3d
ChiefLopes <chieflopesekede@gmail.com>
2023-09-13 17:52:55 +0100

    added the header section

--- my-movie-app/src/App.js
+++ my-movie-app/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Home from "./Components/Home";
 import Header from "./Components/Header/Header";
-
+import Socials from "./Components/Socials";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Socials />
     </div>
   );
 }
---
+++ my-movie-app/src/Components/Socials.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import facebook from "../assets/svgs/facebook.svg";
+import instagram from "../assets/svgs/instagram.svg";
+import twitter from "../assets/svgs/twitter.svg";
+import youtube from "../assets/svgs/youtube.svg";
+const Socials = () => {
+  return (
+    <div className="socials">
+      <img className="facebook" src={facebook}  alt=""></img>
+      <img className="instagram" src={instagram} alt=""></img>
+      <img className="twitter" src={twitter} alt=""></img>
+      <img className="facebook" src={youtube}  alt=""></img>
+     
+    </div>
+  );
+};
+
+export default Socials;
